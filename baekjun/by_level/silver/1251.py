249915116fdfc1bede0793d810ec79b91dcd3b3c
dowoonlee <dwlee717@gmail.com>
2022-01-25 15:07:24 +0900

    sil1251

--- baekjun/by_level/silver/1251.py
+++ baekjun/by_level/silver/1251.py
@@ -1,5 +1,14 @@
 import sys
 
 x = str(sys.stdin.readline().rstrip())
-nx = [ord(i) for i in x]
-print(nx)
+
+ans = x[0] + x[1] + x[2::][::-1]
+
+
+for i in range(1, len(x)-1, 1):
+    for j in range(i+1, len(x),1):
+        nx = x[0:i][::-1] + x[i:j][::-1] + x[j::][::-1]
+        ans = sorted([ans, nx])[0]
+
+print(ans)
+

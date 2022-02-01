a259c909bc23a51e5102e49ed5a8d9ac1be952bc
dowoonlee <dwlee717@gmail.com>
2022-02-01 23:46:16 +0900

    bingo

--- baekjun/by_level/silver/2578.py
+++ baekjun/by_level/silver/2578.py
@@ -1,43 +1,48 @@
 import sys
 
-bingo = []
+board = []
 for _ in range(5):
-    bingo.append(list(map(int, sys.stdin.readline().split())))
+    board.append(list(map(int, sys.stdin.readline().split())))
 
-bx, by, bd = [0]*5, [0]*5, [0]*2
-finalcond = False
+breakcond = False
 for i in range(5):
-    given = list(map(int,sys.stdin.readline().split()))
-    for j, number in enumerate(given):
-        curb=0
-        breakcond = False
+    ipt = list(map(int, sys.stdin.readline().split()))
+    for j in range(5):
+
+
+        for bx in range(5):
+            for by in range(5):
+                if board[bx][by]==ipt[j]:
+                    board[bx][by] = 0
+                    break
+
+        cnt = 0
         for x in range(5):
-            for y in range(5):
-                if bingo[x][y]==number:
-                    bx[x]+=1
-                    by[y]+=1
-                    if x==y:
-                        bd[0]+=1
-                    if x+y==4:
-                        bd[1]+=1
-                    breakcond = True
-            if breakcond:
-                break
+            if sum(board[x]) == 0:
+                cnt += 1
+        for y in range(5):
+            temp = 0
+            for t in range(5):
+                temp += board[t][y]
+            if temp==0:
+                cnt += 1
+        diag1, diag2 = 0, 0
         for k in range(5):
-            if bx[k]==5:
-                curb+=1
-            if by[k]==5:
-                curb+=1
-        for k in range(2):
-            if bd[k]==5:
-                curb+=1
-        if curb==3:
+            diag1 += board[k][k]
+            diag2 += board[k][4-k]
+        if diag1==0:
+            cnt+=1
+        if diag2==0:
+            cnt+=1
+
+        if cnt>=3:
             ans = 5*i+j+1
-            finalcond=True
+            breakcond = True
             break
-    if finalcond:
+    if breakcond:
         break
 print(ans)
+
 
 
 

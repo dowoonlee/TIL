0530cc81942e76e296ff398aef83097be1ff5e7c
dowoonlee <dwlee717@gmail.com>
2022-04-02 12:57:19 +0900

    update

--- baekjun/by_level/silver/17626.py
+++ baekjun/by_level/silver/17626.py
@@ -2,9 +2,15 @@ import sys
 
 n = int(sys.stdin.readline())
 
-sq = []
-i = 1
-while i**2<n:
-    sq.append(i**2)
-    i+=1
-print(sq)
+dp = [0]*(n+1)
+dp[1] = 1
+
+for i in range(2, n + 1):
+    minV = sys.maxsize
+    j = 1
+
+    while (j ** 2) <= i:
+        minV = min(minV, dp[i - (j ** 2)])
+        j += 1
+    dp[i] = minV+1
+print(dp[n])

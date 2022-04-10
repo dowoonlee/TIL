860901ca5464ebfd880dfd1ff36f8fabccc7bd97
dowoonlee <dwlee717@gmail.com>
2022-04-10 09:21:07 +0900

    update

---
+++ baekjun/by_level/silver/6064.py
@@ -0,0 +1,42 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+
+def ex_euclid(a, b):
+    x0, x1, y0, y1 = 1, 0, 0, 1
+
+    while b:
+        n, a, b = a // b, b, a % b
+        x0, x1 = x1, x0 - n * x1
+        y0, y1 = y1, y0 - n * y1
+    return x0, y0
+
+
+for _ in range(t):
+    m, n, x, y = map(int, sys.stdin.readline().split())
+
+    g = gcd(m, n)
+
+    if x % g != y % g:
+        print(-1)
+
+    else:
+        l = lcm(m, n)
+        p, q = ex_euclid(m, n)
+
+
+        test = (m//g)*p + (n//g)*q
+
+        ans = (x * (n // g) * q + y * (m // g) * q) % l
+        print(ans)
--- baekjun/test.py
+++ baekjun/test.py
@@ -1,33 +1,23 @@
 import sys
 
 ###한 개의 정수를 입력받을 때
-#n = int(sys.stdin.readline())
+# n = int(sys.stdin.readline())
 
 ###정해진 개수의 정수를 한줄에 입력받을 때
-#a, b = map(int, sys.stdin.readline().split())
+# a, b = map(int, sys.stdin.readline().split())
 
 ###임의의 개수의 정수를 한줄에 입력받아 리스트에 저장할 때
-#data = list(map(int,sys.stdin.readline().split()))
+# data = list(map(int,sys.stdin.readline().split()))
 
 
 ###임의의 개수의 정수를 n줄 입력받아 2차원 리스트에 저장할 때
-#data = []
-#n = int(sys.stdin.readline())
-#for i in range(n):
+# data = []
+# n = int(sys.stdin.readline())
+# for i in range(n):
 #    data.append(list(map(int,sys.stdin.readline().split())))
 
 ###문자열 n줄을 입력받아 리스트에 저장할 때
-#import sys
-#n = int(sys.stdin.readline())
-#data = [sys.stdin.readline().strip() for i in range(n)]
+# import sys
+# n = int(sys.stdin.readline())
+# data = [sys.stdin.readline().strip() for i in range(n)]
 
-
-card = []
-for t in ["a", "b", "c", "d"]:
-    for n in range(1, 14):
-        card.append([t, n])
-
-from itertools import combinations
-
-comb = combinations(card, 5)
-print(list(comb)[0])
